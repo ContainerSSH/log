8418e89d66c0aac2f81ca62cd0d66afe85a0c840
Janos Pasztor <janos@pasztor.at>
2020-12-11 01:33:52 +0100

    YAML and JSON marshalling
    In the previous release we did not consider the need for also marshalling log level values. This release adds the marshalling method to encode log levels to their string representations in YAML and JSON. It also adds the ability to unmarshal from the numeric values should they be present.

--- config_test.go
+++ config_test.go
@@ -19,6 +19,15 @@ func TestJSONDecode(t *testing.T) {
 	assert.Equal(t, log.FormatText, config.Format)
 }
 
+func TestJSONDecodeNumeric(t *testing.T) {
+	cfg := "{\"level\":7,\"format\":\"text\"}"
+	config := log.Config{}
+	err := json.Unmarshal([]byte(cfg), &config)
+	assert.NoError(t, err)
+	assert.Equal(t, log.LevelDebug, config.Level)
+	assert.Equal(t, log.FormatText, config.Format)
+}
+
 func TestYAMLDecode(t *testing.T) {
 	cfg := "---\nlevel: debug\nformat: text\n"
 	config := log.Config{}
@@ -26,4 +35,43 @@ func TestYAMLDecode(t *testing.T) {
 	assert.NoError(t, err)
 	assert.Equal(t, log.LevelDebug, config.Level)
 	assert.Equal(t, log.FormatText, config.Format)
+}
+
+func TestYAMLDecodeNumeric(t *testing.T) {
+	cfg := "---\nlevel: 7\nformat: text\n"
+	config := log.Config{}
+	err := yaml.Unmarshal([]byte(cfg), &config)
+	assert.NoError(t, err)
+	assert.Equal(t, log.LevelDebug, config.Level)
+	assert.Equal(t, log.FormatText, config.Format)
+}
+
+func TestJSONEncode(t *testing.T) {
+	config := log.Config{
+		Level:  log.LevelDebug,
+		Format: log.FormatText,
+	}
+	jsonData, err := json.Marshal(config)
+	assert.NoError(t, err)
+	rawData := map[string]interface{}{}
+	err = json.Unmarshal(jsonData, &rawData)
+	assert.NoError(t, err)
+
+	assert.Equal(t, "debug", rawData["level"])
+	assert.Equal(t, "text", rawData["format"])
+}
+
+func TestYAMLEncode(t *testing.T) {
+	config := log.Config{
+		Level:  log.LevelDebug,
+		Format: log.FormatText,
+	}
+	jsonData, err := yaml.Marshal(config)
+	assert.NoError(t, err)
+	rawData := map[string]interface{}{}
+	err = yaml.Unmarshal(jsonData, &rawData)
+	assert.NoError(t, err)
+
+	assert.Equal(t, "debug", rawData["level"])
+	assert.Equal(t, "text", rawData["format"])
 }
